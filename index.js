66d16ac54df300e822821d4a0a72dfd601cbeaaf
jm2dw <jm2dw@icloud.com>
2017-02-21 19:04:55 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+var total = 0
+function addmushroomFunction()
+{
+total = total +1
+document.getElementById('totalprice').innerHTML = total;
+}
+
+function addpepperoniFunction()
+{
+total = total +3
+document.getElementById('totalprice').innerHTML = total;
+}
+
+function addpepperFunction()
+{
+total = total +2
+document.getElementById('totalprice').innerHTML = total;
+}
+
+function clearFunction()
+{
+total = 0
+document.getElementById('totalprice').innerHTML = total;
+}
